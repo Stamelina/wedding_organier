17ea3ef678e56ee9ceaec6b739917ca2c20034af
Leena <stamelina@gmail.com>
2016-11-21 22:18:44 +0000

    Finish user edit, update, index, and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -40,4 +40,11 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+    
 end
+    def logged_in_user
+      unless logged_in?
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+    end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-Hotel.create(:name => 'Wedding Venues',
-:description =>
-%{<p> The finest wedding venues in Ireland</p>},
-    :image_url => 'images/h1.jpg')
+User.create!(name:  "Example User",
+             email: "example@railstutorial.org",
+             password:              "foobar",
+             password_confirmation: "foobar",
+             admin: true)
+
+__END__
